846f1f10685ce55ca41e94d91422c9ab0a4e022e
David Bustos <dbustos28@amarillocollege.com>
2025-03-30 22:10:02 -0500

    first commit

--- Main.java
+++ Main.java
@@ -1,54 +1,63 @@
 import java.util.Scanner;  // Import the Scanner class
+
+class candidateName{ // Names for each candidate to vote
+    String candidate1 = "Trump";
+    String candidate2 = "Biden";
+    String candidate3 = "Obama";
+    
+public candidateName(String trump, String biden, String obama){
+    candidate1 = trump;
+    candidate2 = biden;
+    candidate3 = obama;
+}
+
 class Main {
-class GradeCalculator{
-            // Method to calculate grade for assignments
-            public static double CalculateAssignmentsGrade(double assignmentsScore)
-            {
-                return assignmentsScore;
-            }
+  public static void main(String[] args) {
+    while (true){ // repeat forever
 
-            // Method to calculate grade for midterms
-            public static double CalculateMidtermsGrade(double midtermsScore)
-            {
-                return midtermsScore;
-            }
+    Scanner vote = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Enter a number that you wish to vote for from the list below\n" +
+    "1. Trump\n" + 
+    "2. Biden\n" +
+    "3. Obama\n" +
+    "4. dispaly all votes\n" +
+    "5. Exit");
 
-            // Method to calculate grade for final exams
-            public static double CalculateFinalExamsGrade(double finalExamsScore)
-            {
-                return finalExamsScore;
-            }
+    int candidateName = vote.nextInt();
+    switch (candidateName) {
+        case 1:
+            // add one vote for trump
+            String candidate1 = vote.nextLine();
+            System.out.println("vote has succefully been added");
+            break;
+        case 2:
+            // add one vote for biden
+            String candidate2 = vote.nextLine();
+            System.out.println("vote has succefully been added");
+            break;
+        case 3:
+            // add one vote for obama
+            String candidate3 = vote.nextLine();
+            System.out.println("vote has succefully been added");
+            break;
+        case 4:
+            //display all votes
+            String Allcandidate = vote.nextLine();
+            System.out.println("Here is all the votes for each candidate" + candidate1 + candidate2 + candidate3);
+            break;
+        case 5:
+            // exit
+            return;
+        default:
+            System.out.println("Invaild option");
+            break;
+    }
+    
 
-            // Method to calculate the final grade based on weighted average
-            public static double CalculateFinalGrade(double assignmentsScore, double midtermsScore, double finalExamsScore)
-            {
-                double assignmentsWeight = 0.40; // 40% for the assignment
-                double midtermsWeight = 0.30; // 30% for the mid term
-                double finalExamsWeight = 0.30; // 30% Final exam
 
-                double finalGrade = (assignmentsScore * assignmentsWeight) + (midtermsScore * midtermsWeight) + (finalExamsScore * finalExamsWeight);
-                return finalGrade;
-            }
-                public static void Main(String[] args){
-                // Allow user to input scores
-                Scanner Convert = new Scanner(System.in);
-                
-                System.out.println("Enter the score for assignments:");
-                double assignmentsScore = Convert.nextDouble();
-
-                System.out.println("Enter the score for midterms:");
-                double midtermsScore = Convert.nextDouble();
-
-
-                System.out.println("Enter the score for final exams:");
-                double finalExamsScore = Convert.nextDouble();
-
-
-                // Calculate the final grade
-                double finalGrade = CalculateFinalGrade(assignmentsScore, midtermsScore, finalExamsScore);
-
-                // Display the final grade
-                System.out.println("Final Grade: " + finalGrade);
-            }
-         }
-     }
+    }
+    
+  }
+  
+}
+}
